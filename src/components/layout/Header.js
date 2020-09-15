2acd9cd7ec198e9c9a4a6db0ac7d191c73ae6ea1
Aadil <aadil.hasan07@gmail.com>
2020-09-15 22:32:10 +0530

    removed dead, code added logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import LogoIcon from '../../svg/LogoIcon';
 import Button from '../Button';
 
 const Header = () => (
@@ -8,23 +6,9 @@ const Header = () => (
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
         <div className="w-12 mr-3">
-          <LogoIcon />
+          <img alt="protab logo" src={require('./logo.png')} />
         </div>
         ProTab
-      </div>
-      <div className="flex mt-4 sm:mt-0">
-        {/* <AnchorLink className="px-4" href="#features">
-          Features
-        </AnchorLink> */}
-        {/* <AnchorLink className="px-4" href="#services">
-          Services
-        </AnchorLink>
-        <AnchorLink className="px-4" href="#stats">
-          Stats
-        </AnchorLink>
-        <AnchorLink className="px-4" href="#testimonials">
-          Testimonials
-        </AnchorLink> */}
       </div>
       <div className="hidden md:block">
         <a
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import Button from '../components/Button';
 import Card from '../components/Card';
-import CustomerCard from '../components/CustomerCard';
-import LabelText from '../components/LabelText';
 import Layout from '../components/layout/Layout';
-import SplitSection from '../components/SplitSection';
-import StatsBox from '../components/StatsBox';
-import customerData from '../data/customer-data';
-import heroImage from '../images/hero.png';
-import SvgCharts from '../svg/SvgCharts';
 import './styles.css';
 
 const features = [
@@ -50,7 +43,7 @@ export default () => (
       <div className="container mx-auto px-8 lg:flex lg:flex-col">
         <div className="text-center">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none heading">
-            A productivity chrome extension
+            The best productivity extension
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light spa sub-heading">
             Boost your productivity with a highly customizable new tab. Have easy and fast access to
@@ -83,7 +76,7 @@ export default () => (
         <div className="feature-list sm:flex-row sm:-mx-3 mt-12">
           {features.map(feature => {
             return (
-              <div className="flex-1 px-4">
+              <div className="px-4">
                 <Card>
                   <p className="font-semibold text-xl">{feature.title}</p>
                   <p className="mt-4">{feature.details}</p>
@@ -95,81 +88,8 @@ export default () => (
         </div>
       </div>
     </section>
-    {/* <SplitSection
-      id="services"
-      primarySlot={
-        <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">Market Analysis</h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-            Our team of enthusiastic marketers will analyse and evaluate how your company stacks
-            against the closest competitors
-          </p>
-        </div>
-      }
-      secondarySlot={<SvgCharts />}
-    />
-    <SplitSection
-      reverseOrder
-      primarySlot={
-        <div className="lg:pl-32 xl:pl-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-            Design And Plan Your Business Growth Steps
-          </h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-            Once the market analysis process is completed our staff will search for opportunities
-            that are in reach
-          </p>
-        </div>
-      }
-      secondarySlot={<SvgCharts />}
-    />
-    <SplitSection
-      primarySlot={
-        <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-            Search For Performance Optimization
-          </h3>
-          <p className="mt-8 text-xl font-light leading-relaxed">
-            With all the information in place you will be presented with an action plan that your
-            company needs to follow
-          </p>
-        </div>
-      }
-      secondarySlot={<SvgCharts />}
-    />
-    <section id="stats" className="py-20 lg:pt-32">
-      <div className="container mx-auto text-center">
-        <LabelText className="text-gray-600">Our customers get results</LabelText>
-        <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
-          <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
-          </div>
-          <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
-          </div>
-          <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
-          </div>
-        </div>
-      </div>
-    </section>
-    <section id="testimonials" className="py-20 lg:py-40">
-      <div className="container mx-auto">
-        <LabelText className="mb-8 text-gray-600 text-center">What customers are saying</LabelText>
-        <div className="flex flex-col md:flex-row md:-mx-3">
-          {customerData.map(customer => (
-            <div key={customer.customerName} className="flex-1 px-3">
-              <CustomerCard customer={customer} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </section> */}
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
       <h4 className="text-5xl font-semibold">Are you ready to become more productive ? </h4>
-      {/* <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
-      </p> */}
       <p className="mt-8">
         <div className="hidden md:block">
           <a
